72dc835c9ef6f1f47555a169a630955b35d63c60
Dima <117075948+Dmitri28@users.noreply.github.com>
2022-11-02 12:06:33 +0200

    Structure
    Structure

---
+++ Structure/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Structure
+{
+    class Program
+    {
+        struct Num
+        {
+            public int n;
+            public void show()
+            {
+                Console.WriteLine("You are enered this number"+" " + n);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Num num = new Num();
+            num.n = 10;
+            num.show();
+        }
+    }
+  
+}
+
